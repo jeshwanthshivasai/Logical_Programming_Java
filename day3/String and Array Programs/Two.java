30b5bf193e3aeb3406608921180eeeb1cc860ecd
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-11-14 22:08:27 +0530

    oops forgot to save 2nd one

--- day3/String and Array Programs/Two.java
+++ day3/String and Array Programs/Two.java
@@ -1,5 +1,13 @@
 package day3.stringandarrayprograms;
-
+//print elements in reverse order
 public class Two {
-
+	public static void reverseOrder(String[] array) {
+        for(int i = array.length-1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+	public static void main(String[] args) {
+        String[] array = {"jess", "jesse", "jesse james", "jesse frankfurt"};
+        reverseOrder(array);
+    }
 }
