a40ab614876371ddd46e2dcb3e758624865ba3a4
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-11-14 21:59:15 +0530

    oops forgot to save the 5th

--- day3/Standard Programs/Five.java
+++ day3/Standard Programs/Five.java
@@ -1,5 +1,9 @@
 package day3.standardprograms;
-
+//print alphabets from A to Z
 public class Five {
-
+	public static void main(String[] args) {
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c + " ");
+        }
+    }
 }
